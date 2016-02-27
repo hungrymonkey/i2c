a3624ef9b6404bbe7eb79114b2db7e92a1eb311f
Ted L Chang <tlc008@ieng6-240.ucsd.edu>
2016-02-26 18:48:28 -0800

    it compiles'

--- validate.vhd
+++ validate.vhd
@@ -7,25 +7,25 @@ entity validate is
            sclIn   : in STD_LOGIC;
            sdaOut  : out STD_LOGIC;
            start   : out STD_LOGIC;
-           stop    : out STD_LOGIC;
+           stop    : out STD_LOGIC
         );
-
-architecture lab3 of validate is
+end validate;
+architecture lab3 of validate is begin
 	process(sdaIn,sclIn) begin
 		if( falling_edge(sdaIn)) and ( sclIn = '1') then
-			start = '1';
-			stop = '0';
+			start <= '1';
+			stop <=  '0';
 		else
-			start = '0';
-			stop = '0';
+			start <= '0';
+			stop <= '0';
 		end if;
 		if( rising_edge(sdaIn)) and ( sclIn = '1') then
-			stop = '1'; 
-			start = '0';
+			stop <= '1'; 
+			start <= '0';
 
 		else
-			stop = '0';
-			start = '0';
+			stop <= '0';
+			start <= '0';
 		end if;
 		sdaOut <= sdaIn;
 	end process;
