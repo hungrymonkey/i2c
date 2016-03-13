6b0d617101ef1251668a3860b11933671a211b8e
hungrymonkey <boringmonkey@gmail.com>
2016-03-13 12:49:51 -0700

    Added files via upload

--- CO_Proc.vhdl
+++ CO_Proc.vhdl
@@ -4,14 +4,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 ENTITY CO_PROC IS
 	GENERIC(bus_clk   : INTEGER := 1_000_000;
 	        sys_clk   : INTEGER := 1000000000 ;
-			slave_addr : std_logic_vector( 6 DOWNTO 0) := "0101010");
+			slave_addr : std_logic_vector( 6 DOWNTO 0) := "0101010";
+			N : INTEGER := 1920;
+			P : INTEGER := 1920);
+
 	PORT(
 		CLK 	: IN STD_LOGIC;
 		RESET	: IN STD_LOGIC;
 --		RAM		:  inout ram_type;
 		SDA 	: INOUT STD_LOGIC;
-		SCL 	: IN STD_LOGIC
---		P		: OUT STD_LOGIC_VECTOR( 15 DOWNTO 0 )
+		SCL 	: IN STD_LOGIC;
+		DONE	: OUT STD_LOGIC;
+		D_OUT	: OUT STD_LOGIC_VECTOR( 7 DOWNTO 0);
+		WEN	: OUT STD_LOGIC
+
 	);
 END ENTITY;
 ARCHITECTURE arch OF CO_PROC IS
@@ -22,7 +28,6 @@ ARCHITECTURE arch OF CO_PROC IS
 	PORT(
 		CLK 	: IN STD_LOGIC;
 		RESET	: IN STD_LOGIC;
---		RAM		:  inout ram_type;
 		SDA 	: INOUT STD_LOGIC;
 		SCL 	: IN STD_LOGIC;
 		P		: OUT STD_LOGIC_VECTOR( 15 DOWNTO 0 )
@@ -62,7 +67,16 @@ ARCHITECTURE arch OF CO_PROC IS
 	);
 	END COMPONENT;
 	SIGNAL p_num : STD_LOGIC_VECTOR( 15 DOWNTO 0 ) := X"0000";
+	SIGNAL M1_ADDR_A, M1_ADDR_B : INTEGER := 1;
+	SIGNAL M1_DA, M1_DB : STD_LOGIC_VECTOR( 7 DOWNTO 0 ) := X"00";
+
+	SIGNAL M2_ADDR_A, M2_ADDR_B : INTEGER := 1;
+	SIGNAL M2_DA, M2_DB : STD_LOGIC_VECTOR( 7 DOWNTO 0 ) := X"00";
 
 BEGIN
 	i2c: I2C_SLAVE GENERIC MAP( bus_clk => bus_clk, sys_clk => sys_clk , slave_addr=>slave_addr) PORT MAP( CLK => CLK , RESET=> reset , SDA => SDA, SCL=> SCL, P => p_num);
+	
+	mat1: BRAM_MAT1  PORT MAP( EN_A => '1', CLK_A => CLK, ADDR_R_A => M1_ADDR_A, D_A => M1_DA, EN_B => '1', CLK_B => CLK, ADDR_R_B => M1_ADDR_B, D_B => M1_DB );
+	mat2: BRAM_MAT2  PORT MAP( EN_A => '1', CLK_A => CLK, ADDR_R_A => M2_ADDR_A, D_A => M2_DA, EN_B => '1', CLK_B => CLK, ADDR_R_B => M2_ADDR_B, D_B => M2_DB );
+
 END ARCHITECTURE arch;
